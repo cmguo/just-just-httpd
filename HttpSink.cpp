ee704f5272aafdd58146b4ee281ad1a63a32d0bb
zenzhang <zenzhang@pptv.com>
2012-10-12 08:13:02 +0000

    1.2.0/src/ppbox/httpd/HttpSink.cpp 未包含日志头文件
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@5200 b3a1389e-2159-5c43-9038-463bf877d0d9

--- HttpSink.cpp
+++ HttpSink.cpp
@@ -5,6 +5,10 @@
 
 #include <util/protocol/http/HttpSocket.h>
 
+#include <framework/logger/Logger.h>
+#include <framework/logger/StreamRecord.h>
+using namespace framework::logger;
+
 FRAMEWORK_LOGGER_DECLARE_MODULE_LEVEL("ppbox.httpd.HttpSink", Debug)
 
 namespace ppbox
