81b283f6f376a4e4715ef0cda56a9c18ffdd248c
isxxguo <isxxguo@pptv.com>
2012-10-29 09:45:38 +0000

    /ppbox/branches/1.2.0 /src/ppbox/httpd 完善应答HTTP头
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@5354 b3a1389e-2159-5c43-9038-463bf877d0d9

--- HttpSession.cpp
+++ HttpSession.cpp
@@ -14,6 +14,8 @@
 #include <ppbox/common/CommonUrl.h>
 
 #include <util/protocol/http/HttpSocket.h>
+#include <util/archive/XmlOArchive.h>
+#include <util/serialization/ErrorCode.h>
 using namespace util::protocol;
 
 #include <framework/string/Base64.h>
@@ -21,12 +23,31 @@ using namespace util::protocol;
 #include <framework/logger/StreamRecord.h>
 using namespace framework::logger;
 
-#include <tinyxml/tinyxml.h>
-
 #include <boost/lexical_cast.hpp>
 #include <boost/bind.hpp>
 
 FRAMEWORK_LOGGER_DECLARE_MODULE_LEVEL("ppbox.httpd.HttpSession", Debug);
+
+namespace util
+{
+    namespace serialization
+    {
+
+        template <
+            typename Archive
+        >
+        void serialize(
+            Archive & ar, 
+            ppbox::data::MediaInfo & info)
+        {
+            ar & SERIALIZATION_NVP_1(info, file_size);
+            ar & SERIALIZATION_NVP_1(info, duration);
+            ar & SERIALIZATION_NVP_1(info, bitrate);
+            ar & SERIALIZATION_NVP_1(info, is_live);
+        }
+
+    }
+}
 
 namespace ppbox
 {
@@ -37,7 +58,6 @@ namespace ppbox
             HttpManager & mgr)
             : HttpServer(mgr.io_svc())
             , mgr_(mgr)
-            , seek_(-1)
             , dispatcher_(NULL)
         {
         }
@@ -75,21 +95,34 @@ namespace ppbox
                     && option != "/play") {
                         ec = framework::system::logic_error::invalid_argument;
                 }
+
+                if (option == "/play") {
+                    if(request().head().range.is_initialized()) {
+                        util::protocol::http_field::RangeUnit unit = 
+                            request().head().range.get()[0];
+                        seek_range_.type = ppbox::dispatch::SeekRange::byte;
+                        seek_range_.beg = unit.begin();
+                        if (unit.has_end()) {
+                            seek_range_.end = unit.end();
+                        }
+                    } else if (!url_.param("start").empty()) { // 伪HTTP协议
+                        seek_range_.type = ppbox::dispatch::SeekRange::time;
+                        seek_range_.beg = framework::string::parse<boost::uint64_t>(url_.param("start"));
+                    }
+                }
             }
 
             if (ec) {
-                make_error_response_body(body_,ec);
-                resp(ec,body_.size());
+                make_error(ec);
+                resp(ec, response_data().size());
                 return;
             }
 
             dispatcher_ = mgr_.dispatch_module().alloc_dispatcher();
 
             dispatcher_->async_open(url_, 
-                boost::bind(&HttpSession::on_open, this,resp, _1));
+                boost::bind(&HttpSession::handle_open, this,resp, _1));
         }
-
-        static void nop_resp(boost::system::error_code const & ec) {}
 
         void HttpSession::transfer_response_data(
             response_type const & resp)
@@ -98,29 +131,12 @@ namespace ppbox
             
             boost::system::error_code ec;
                 
-            if (!body_.empty()) {
-                std::cout << body_ << std::endl;
-                size_t tSize = body_.size();
-                write_some(boost::asio::buffer(body_), ec);
-                body_.clear();
-                resp(ec, tSize);
+            if (response_data().size()) {
+                HttpServer::transfer_response_data(resp);
             } else {
                 assert(url_.path() == "/play");
-                ppbox::dispatch::SeekRange range;
-                if(request().head().range.is_initialized()) {
-                    util::protocol::http_field::RangeUnit unit = 
-                        request().head().range.get()[0];
-                    range.type = ppbox::dispatch::SeekRange::byte;
-                    range.beg = unit.begin();
-                    if (unit.has_end()) {
-                        range.end = unit.end();
-                    }
-                } else if (!url_.param("start").empty()) { // 伪HTTP协议
-                    range.type = ppbox::dispatch::SeekRange::time;
-                    range.beg = framework::string::parse<boost::uint64_t>(url_.param("start"));
-                }
-                dispatcher_->async_play(range, nop_resp, 
-                    boost::bind(&HttpSession::on_playend,this, resp, _1));
+                dispatcher_->async_play(seek_range_, ppbox::dispatch::response_t(), 
+                    boost::bind(&HttpSession::handle_play,this, resp, _1));
             }
         }
 
@@ -133,86 +149,125 @@ namespace ppbox
             boost::system::error_code const & ec)
         {
             LOG_INFO("[on_error] dispatcher:" << dispatcher_ << " ec:" << ec.message());
+            boost::system::error_code ec1;
+            if (dispatcher_) {
+                dispatcher_->close(ec1);
+            }
+        }
+
+        char const * format_mine[][2] = {
+            {"flv", "video/x-flv"}, 
+            {"ts", "video/MP2T"}, 
+            {"m3u8", "application/x-mpegURL"},
+        };
+
+        char const * content_type(
+            std::string const & format)
+        {
+            for (size_t i = 0; i < sizeof(format_mine) / sizeof(format_mine[0]); ++i) {
+                if (format == format_mine[i][0]) {
+                    return format_mine[i][1];
+                }
+            }
+            return "video";
         }
 
         //Dispatch 线程
-        void HttpSession::on_open(
+        void HttpSession::handle_open(
             response_type const & resp,
             boost::system::error_code const & ec)
         {
-            LOG_INFO( "[on_open] dispatcher:" << dispatcher_ << " ec:" << ec.message());
+            LOG_INFO( "[handle_open] dispatcher:" << dispatcher_ << " ec:" << ec.message());
 
             boost::system::error_code ec1 = ec;
             std::string option = url_.path();
+
+            ppbox::data::MediaInfo info;
 
             if (!ec1) {
                 if (option == "/play") {
                     if (url_.param("chunked") == "true") {
                         response_head()["Transfer-Encoding"]="{chunked}";
                     }
-                    dispatcher_->setup(-1, response_stream(), ec1);
+                    dispatcher_->setup(-1, response_stream(), ec1)
+                        && dispatcher_->get_media_info(info, ec1);
                 }
             }
 
             if (ec1) {
-                make_error_response_body(body_,ec1);
-                resp(ec1, body_.size());
+                make_error(ec1);
+                resp(ec1, response_data().size());
                 return;
             }
 
-            if (option == "mediainfo") {
-                make_mediainfo(ec1);
-                resp(ec1, body_.size());
-            } else if (option == "playinfo") {
-                make_playinfo(ec1);
-                resp(ec1, body_.size());
+            if (option == "/play") {
+                response_head()["Content-Type"] = std::string("{") + content_type(info.format) + "}";
+                if (info.file_size == ppbox::data::invalid_size) {
+                    resp(ec1, Size());
+                } else {
+                    if (seek_range_.type == ppbox::dispatch::SeekRange::byte) {
+                        util::protocol::http_field::ContentRange content_range(info.file_size, seek_range_.beg, seek_range_.end);
+                        response_head().content_range = content_range;
+                        resp(ec1, Size((size_t)(info.file_size - seek_range_.beg)));
+                    } else {
+                        resp(ec1, Size((size_t)info.file_size));
+                    }
+                }
             } else {
-                
+                response_head()["Content-Type"]="{application/xml}";
+                if (option == "/mediainfo") {
+                    make_mediainfo(ec1);
+                } else if (option == "/playinfo") {
+                    make_playinfo(ec1);
+                }
+                if (ec1) {
+                    make_error(ec1);
+                }
+                resp(ec1, response_data().size());
             }
         }
 
-        void HttpSession::on_playend(
+        void HttpSession::handle_play(
             response_type const & resp,
             boost::system::error_code const & ec)
         {
-            LOG_INFO( "[on_playend] ec:"<<ec.message());
+            LOG_INFO( "[handle_play] ec:"<<ec.message());
             resp(ec, 0);
         }
 
-        void HttpSession::make_error_response_body(
-            std::string& respone_str, 
-            boost::system::error_code const & last_error)
+        void HttpSession::make_error(
+            boost::system::error_code const & ec)
         {
 
             response_head().err_code = 500;
             response_head().err_msg = "Internal Server Error";
             response_head()["Content-Type"]="{application/xml}";
 
-            respone_str = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<root>\r\n<category>";
-            // set error code value
-            respone_str += last_error.category().name();
-            respone_str += "</category>\r\n<vaule>";
-            respone_str += framework::string::format(last_error.value());
-            respone_str += "</vaule>\r\n<message>";
-            // set error msg
-            if (last_error) {
-                respone_str += last_error.message();
-            } else {
-                respone_str += "";
-            }
-            respone_str += "</message>\r\n</root>";
+            util::archive::XmlOArchive<> oa(response_data());
+            oa << ec;
+            oa << SERIALIZATION_NVP_NAME("message", ec.message());
         }
 
         void HttpSession::make_mediainfo(
-            boost::system::error_code& ec)
+            boost::system::error_code & ec)
         {
-            response_head()["Content-Type"]="{application/xml}";
+            ppbox::data::MediaInfo info;
+            dispatcher_->get_media_info(info, ec);
+            if (!ec) {
+                util::archive::XmlOArchive<> oa(response_data());
+                oa << info;
+            }
         }
 
         void HttpSession::make_playinfo(
             boost::system::error_code& ec)
         {
-            response_head()["Content-Type"]="{application/xml}";
+            ppbox::data::MediaInfo info;
+            dispatcher_->get_play_info(info, ec);
+            if (!ec) {
+                util::archive::XmlOArchive<> oa(response_data());
+                oa << info;
+            }
         }
 
     } // namespace httpd
--- HttpSession.h
+++ HttpSession.h
@@ -3,11 +3,11 @@
 #ifndef _PPBOX_HTTPD_HTTP_SESSION_H_
 #define _PPBOX_HTTPD_HTTP_SESSION_H_
 
+#include <ppbox/dispatch/DispatchBase.h>
+
 #include <util/protocol/http/HttpServer.h>
 #include <util/protocol/http/HttpRequest.h>
 #include <util/protocol/http/HttpResponse.h>
-
-#include <framework/string/Url.h>
 
 namespace ppbox
 {
@@ -42,37 +42,37 @@ namespace ppbox
             virtual void local_process(response_type const & resp);
 
         private:
-            void on_open(response_type const &resp,
+            void handle_open(
+                response_type const & resp,
                 boost::system::error_code const & ec);
 
-            void on_playend(response_type const &resp,
+            void handle_play(
+                response_type const & resp,
                 boost::system::error_code const & ec);
 
-            void make_error_response_body(
-                std::string& respone_str,
-                boost::system::error_code const & last_error);
+            void make_error(
+                boost::system::error_code const & ec);
 
-            void m3u8_protocol(boost::system::error_code& ec);
+            void make_mediainfo(
+                boost::system::error_code& ec);
 
-            void make_mediainfo(boost::system::error_code& ec);
-            void make_playinfo(boost::system::error_code& ec);
+            void make_playinfo(
+                boost::system::error_code& ec);
+
+            void m3u8_protocol(
+                boost::system::error_code& ec);
+
             void make_m3u8(boost::system::error_code& ec);
 
         private:
             HttpManager & mgr_;
-
             framework::string::Url url_;
-
-            std::string body_;
-            std::string host_;
-            boost::uint32_t seek_;
-
             ppbox::dispatch::DispatcherBase * dispatcher_;
             std::vector<util::stream::Sink*> sinks_;
+            ppbox::dispatch::SeekRange seek_range_;
         };
 
     } // namespace httpd
-
 } // namespace ppbox
 
 #endif // _PPBOX_HTTPD_HTTP_SESSION_H_
